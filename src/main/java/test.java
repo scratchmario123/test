26c2f46acf0817ca8ee57d181a58659604443623
scratchmario123 <edmondhui2000@gmail.com>
2021-07-28 09:22:02 +0800

    removed line 5

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -2,6 +2,7 @@ public class test {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("This is a test project");
-        System.out.println("yo");
+
+
     }
 }
