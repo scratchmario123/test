7840fcffe3cc6bbaa2bcabef4455d57e4fe8f929
scratchmario123 <edmondhui2000@gmail.com>
2021-07-28 09:16:45 +0800

    Revert "Added a new description"
    This reverts commit b6a17858

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -2,6 +2,5 @@ public class test {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("This is a test project");
-        System.out.println("test");
     }
 }
