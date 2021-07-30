604013fea0c4a34308f527d05ad4d6ef1075dec0
scratchmario123 <edmondhui2000@gmail.com>
2021-07-30 15:41:13 +0800

    changed name

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,7 +1,7 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("This is a test project");
+        System.out.println("This is a no");
 
 
     }
